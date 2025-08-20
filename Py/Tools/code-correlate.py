cc9264733634222a3dfd4b42eed1b60905d06351
Guy Fedorkow <guy.fedorkow@gmail.com>
2025-08-20 08:19:50 -0400

    more work on refining the code-correlate tool; fixed a simple but serious bug in the comparison of words from the probe with the archive; disassembled the words that match

--- Py/Tools/code-correlate.py
+++ Py/Tools/code-correlate.py
@@ -48,6 +48,8 @@ sorted(student_objects, key=lambda student: student.age)   # sort by age
 import argparse
 import wwinfra
 
+cb = wwinfra.ConstWWbitClass()
+
 def breakp():
     print("breakp")
 
@@ -62,18 +64,62 @@ class SettingsClass:
 
 
 class MatchStat:
-    def __init__(self, run_length=0, offset=0, probe_len=0):
+    def __init__(self, run_length, archive_match_start, probe_offset, offset, probe_len):
         self.run_length = run_length
+        self.archive_match_start = archive_match_start
+        self.probe_offset = probe_offset
         self.offset = offset
         self.probe_len = probe_len
     def __repr__(self):
-        return "run_len=%d, at offset=%d, %3.2f%%" % \
-            (self.run_length, self.offset, 100.0 * self.run_length/self.probe_len)
+        return "run_len=%d, at archive offset=%d (0o%o), probe_offset=%d (0o%o), %3.2f%%" % \
+            (self.run_length, self.archive_match_start, self.archive_match_start, self.probe_offset,
+             self.probe_offset, 100.0 * self.run_length/self.probe_len)
+
+
+def DecodeOp(w, pc, short=False):
+    global cb
+    if w is None:
+        return 'None'
+    if w == 0 or w == 1:
+        # a zero or one could be a halt, but it's almost certainly a constant or initialized variable
+        return ".word 0o%o" % w
+    op = w >> 11
+    op_name = cb.op_code[op][0]
+    wordtype = ''
+    addr = w & cb.WW_ADDR_MASK
+    # if the word is marked as a data word, or if it's an unused op code, don't decode it as an instruction
+    if (wordtype == '.word') | (cb.op_code[op][2] == cb.OPERAND_UNUSED):
+        return "0o%05o:   .word 0o%06o" % (pc, w)
+    operand = "0o%o" % addr  # in these instructions, it's not an address, it's a param, e.g., number of bits ot shift
+
+    long_op = "%3s  %5s" % (cb.op_code[op][0], operand)
+    comment = "%s" % cb.op_code[op][1]
+
+#    if op_name in ["SL", "SR", "CL"]:   # there are three extended 6-bit op-codes; all use "params", not addresses
+#        if w & cb.WWBIT5:  # If this bit is on, it can't be a legal shift instruction; must be a data word
+#            return ".word 0o%06o" % w
+#        ext_op_bit = (w >> 9) & 0o1  # the extra op bit is the "512s" bit, not the MSB of address
+#        long_op = "%3s  0o%05o" % \
+#                  (ext_op_code[op_name][0][ext_op_bit], addr & 0o777)
+#        comment = ext_op_code[op_name][1][ext_op_bit]
+#    elif op_name == "cf":
+#        long_op = "%3s  %5s" % (cb.op_code[op][0], operand)
+#        comment = "cf" + cf_decode(addr)
+    if op_name == "si":
+        comment = "select I/O: " + cb.Decode_IO(addr)
+
+    if short:
+        return op_name
+    long = "0o%05o:   %s   ; %s" % (pc, long_op, comment)
+    return long
+
+
 
 
 def print_match(probe, archive, probe_match_start, archive_match_start, match_length):
-    print("\nMatch: probe start=%d, archive_start=%d, length=%d" % (probe_match_start, archive_match_start, match_length))
-    prt = "archive start=%03d: " % archive_match_start
+    print("\nMatch: probe start=%d (0o%o), archive_start=%d (0o%o), length=%d" %
+          (probe_match_start, probe_match_start, archive_match_start, archive_match_start, match_length))
+    prt = "archive code, starting at 0o%03o:\n" % archive_match_start
     start = archive_match_start # - 10
     probe_offset_from_archive = probe_match_start - archive_match_start
     #if start < 0:
@@ -84,24 +130,25 @@ def print_match(probe, archive, probe_match_start, archive_match_start, match_le
     for offset in range(start, end):
         cell = archive[offset]
         if cell is not None:
-            prt += " %03o" % cell
+#            prt += " 0o%03o" % cell
+            prt += DecodeOp(cell, offset) + '\n'
         else:
             prt += " NaN"
 
-    prt += '\n  probe start=%03d: ' % probe_match_start
-    end = probe_match_start + match_length + 10
-    if end > len(probe):
-        end = len(probe)
-    for s in range(0, probe_offset_from_archive):
-        prt += "    "
-    for s in range(0, end):
-        p = probe[s]
-        if p is not None:
-            prt += " %03o" % p
+#    prt += '\n  probe start=%03d: ' % probe_match_start
+#    end = probe_match_start + match_length + 10
+#    if end > len(probe):
+#        end = len(probe)
+#    for s in range(0, probe_offset_from_archive):
+#        prt += "    "
+#    for s in range(0, end):
+#        p = probe[s]
+#        if p is not None:
+#            prt += " %03o" % p
     print(prt)
 
 
-def scan_archive(sc, scan_start, probe, archive):
+def scan_archive(sc, scan_start, probe, archive, probe_addr_correction):
     probe_offset = 0
     probe_match_start = 0
     archive_match_start = 0
@@ -113,22 +160,29 @@ def scan_archive(sc, scan_start, probe, archive):
         p = probe[probe_offset]
         a = archive[scan_start + probe_offset]
         if not sc.match_complete_word:
-            if p: p &= 0o174
-            if a: a &= 0o174
-        if a is not None and p == a:
+            if p: p &= 0o174000  # keep the WW Op Code; toss the address field
+            if a: a &= 0o174000
+        # Here's the critical line matchine a word from the two files
+        if a is not None and p is not None and p == a:  # don't match None, but if the values match, it's part of a run
             if run == 0:
                 probe_match_start = probe_offset
                 archive_match_start = scan_start + probe_offset
-            run += 1
+            if a != 0:   # only count non-zero words in the run; i.e., don't report blocks of zeros
+                run += 1
         else:
             if run > sc.run_threshold:
-                matches.append(MatchStat(run, scan_start, len(probe)))
-                if sc.chatty:
-                    print_match(probe, archive, probe_match_start, archive_match_start, run)
+                if run == 428:
+                    breakp()
+                matches.append(MatchStat(run, archive_match_start, probe_match_start + probe_addr_correction, scan_start, len(probe)))
+                if True: # sc.chatty:
+                    print_match(probe, archive, probe_match_start + probe_addr_correction, archive_match_start, run)
             run = 0
 
+    # pick up a last run of matches, in case we matched right through to the end
     if run > sc.run_threshold:
-        matches.append(MatchStat(run, scan_start, len(probe)))
+        matches.append(MatchStat(run, archive_match_start, probe_offset, scan_start, len(probe)))
+        if True: # sc.chatty:
+            print_match(probe, archive, probe_match_start + probe_addr_correction, archive_match_start, run)
 
     return matches
 
@@ -143,13 +197,15 @@ def index_scan(sc, probe, archive):
         probe_start = 0
         probe_end = probe_len
         scan_start = offset
+        probe_addr_correction = 0
         if offset < 0:
             probe_start = -offset
+            probe_addr_correction = -offset
             scan_start = 0
         #if offset + probe_len > archive_len:
         #    probe_end = probe_start + probe_len - archive_len
 
-        m = scan_archive(sc, scan_start, probe[probe_start:probe_end], archive)
+        m = scan_archive(sc, scan_start, probe[probe_start:probe_end], archive, probe_addr_correction)
         if len(m):
             if sc.chatty:
                 print("match at offset=%d, probe_end=%d" % (offset, probe_end))
@@ -160,24 +216,36 @@ def index_scan(sc, probe, archive):
     return matches
 
 
+# def coremem_to_list(cb, cm, break_at_none_cell=False):
+#     mem_list = []
+#     first_addr = None
+#     last_addr = 0
+#     for addr in range(0, cb.CORE_SIZE):
+#         cell = cm.rd(addr, fix_none=False)
+#         if first_addr is None:  # skip initial 'none' cells, then remember the first non-none address
+#             if cell is None:
+#                 continue
+#             else:
+#                 first_addr = addr
+#         mem_list.append(cell)
+#         if cell is not None:
+#             last_addr = addr
+#     # print("last_addr=%d, memlist=" % last_addr, mem_list[0:(last_addr - first_addr) + 1])
+#     if first_addr is None:
+#         return []
+#     return mem_list[0:(last_addr - first_addr) + 1]
+
+
 def coremem_to_list(cb, cm, break_at_none_cell=False):
     mem_list = []
-    first_addr = None
     last_addr = 0
     for addr in range(0, cb.CORE_SIZE):
         cell = cm.rd(addr, fix_none=False)
-        if first_addr is None:  # skip initial 'none' cells, then remember the first non-none address
-            if cell is None:
-                continue
-            else:
-                first_addr = addr
         mem_list.append(cell)
         if cell is not None:
             last_addr = addr
     # print("last_addr=%d, memlist=" % last_addr, mem_list[0:(last_addr - first_addr) + 1])
-    if first_addr is None:
-        return []
-    return mem_list[0:(last_addr - first_addr) + 1]
+    return mem_list[0:last_addr]
 
 
 def main():
@@ -247,15 +315,16 @@ def main():
     total_matches = len(matches)
     matches_to_print = 50
     i = 0
-    prt = ("\nTotal of %d matches" % (total_matches))
+    prt = ("\nTotal of %d matches, sorted longest to shortest:" % (total_matches))
     if total_matches > matches_to_print:
         prt += "; first %d are:" % matches_to_print
     if sc.chatty: print(prt)
     for m in matches:
-        print("\nMatch: %s file %s" % (m.__repr__(), sc.archive_filename))
+        print("Match: %s file %s" % (m.__repr__(), sc.archive_filename))
         i += 1
         if i >= matches_to_print:
             break
+    print('')  # newline
     # report results
 
 
