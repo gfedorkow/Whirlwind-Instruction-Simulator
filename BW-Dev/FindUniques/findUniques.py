6417336e71f34a2385147731d26e288365db660c
Brian White <Brian.white@umb.edu>
2025-09-23 13:53:41 -0400

    use Larry's idea for stripping out metadata

--- BW-Dev/FindUniques/findUniques.py
+++ BW-Dev/FindUniques/findUniques.py
@@ -35,12 +35,22 @@ for i in range(len(allFiles)):
     if (allFiles[i]["dup"] == 0):
         # it isn't, so save the name and look for duplicates
         uniqueFiles.append(allFiles[i]["fn"])
-#        print(allFiles[i]["fn"])
+        print(allFiles[i]["fn"])
+        # create a cleaned copy for comparison
+        os.system("grep -v \"%File:\" " + allFiles[i]["fn"] + " > ~/Desktop/wwTemp/temp1")
         for j in range(i + 1, len(allFiles)):
             if (allFiles[j]["dup"] == 0):
+                # create a cleaned copy for comparison
                 print("comparing " + str(i) + " and " + str(j))
-                if(filecmp.cmp(allFiles[i]["fn"], allFiles[j]["fn"], shallow = False)):
+                os.system("grep -v \"%File:\" " + allFiles[j]["fn"] + " > ~/Desktop/wwTemp/temp2")
+                # do the comparison
+                command = "diff ~/Desktop/wwTemp/temp1 ~/Desktop/wwTemp/temp2"
+                process = os.popen(command)
+                result = process.read()
+                process.close()
+                if (result == ""):
                     # if [j] is a duplicate of [i]; mark both as duplicates
+                    print("***found a dup")
                     allFiles[i]["dup"] = 1
                     allFiles[j]["dup"] = 1
                 
