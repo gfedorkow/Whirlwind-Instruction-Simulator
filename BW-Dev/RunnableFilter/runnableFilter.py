f8f5f5cf0895067a42bd7e99dcb1e149e4e78ab2
Brian White <Brian.white@umb.edu>
2025-09-24 16:15:20 -0400

    can tell if file will run or not

--- BW-Dev/RunnableFilter/runnableFilter.py
+++ BW-Dev/RunnableFilter/runnableFilter.py
@@ -8,9 +8,29 @@ Created on Wed Sep 24 15:37:13 2025
 
 import os
 
-command = "python3 ../../Py/Disassembler/wwdisasm.py -o - ../../Recovered-Tapes/Translated-Files/Magnetic-Tapes/132/t132_pt2_good_gs007.ocore"
+#this one won't run
+#command = "python3 ../../Py/Disassembler/wwdisasm.py -o - ../../Recovered-Tapes/Translated-Files/Magnetic-Tapes/04/t04_gs041.tcore"
+
+# this one will
+command = "python3 ../../Py/Disassembler/wwdisasm.py -o - ../../Recovered-Tapes/Translated-Files/Paper-Tapes/2018_01/102766760_fb2353_polynomial_gs001_FB#131-0-2691.tcore"
+
 process = os.popen(command)
 result = process.read()
 process.close();
 
-print(result)
+addressesWithData = []
+jumpToAddr = ""
+for line in result.splitlines():
+	if (line.startswith("@")):
+		addrString = line.split(":")[0].replace("@", "")
+		addressesWithData.append(int(addrString, 8))
+	if (".JumpTo" in line):
+		jumpToAddr = int(line.split(".JumpTo")[1], 8)
+		
+
+if jumpToAddr in addressesWithData:
+	print("will run")
+else:
+	print("won't run")
+	
+	
