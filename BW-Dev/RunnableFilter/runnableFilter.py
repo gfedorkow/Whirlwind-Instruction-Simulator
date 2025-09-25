1ae547c9a2cf00c0ba76056f459ea28d20e3f02e
Brian White <Brian.white@umb.edu>
2025-09-25 16:50:33 -0400

    runnable filter almost working

--- BW-Dev/RunnableFilter/runnableFilter.py
+++ BW-Dev/RunnableFilter/runnableFilter.py
@@ -8,29 +8,47 @@ Created on Wed Sep 24 15:37:13 2025
 
 import os
 
-#this one won't run
-#command = "python3 ../../Py/Disassembler/wwdisasm.py -o - ../../Recovered-Tapes/Translated-Files/Magnetic-Tapes/04/t04_gs041.tcore"
+runnableFiles = []
+nonRunnableFiles = []
+fcFileCount = 0
 
-# this one will
-command = "python3 ../../Py/Disassembler/wwdisasm.py -o - ../../Recovered-Tapes/Translated-Files/Paper-Tapes/2018_01/102766760_fb2353_polynomial_gs001_FB#131-0-2691.tcore"
+with open('uniqueFileNames.txt', 'r') as f:
+	for fileName in f:
+		# .fc files are not readable by the disasm
+		if (not ".fc" in fileName):
+			command = "python3 ../../Py/Disassembler/wwdisasm.py -o - " + fileName
+			
+			process = os.popen(command)
+			result = process.read()
+			process.close();
+			
+			addressesWithData = []
+			jumpToAddr = ""
+			for line in result.splitlines():
+				if (line.startswith("@")):
+					addrString = line.split(":")[0].replace("@", "")
+					addressesWithData.append(int(addrString, 8))
+				if (".JumpTo" in line):
+					jumpToAddr = int(line.split(".JumpTo")[1], 8)
+					
+			
+			if jumpToAddr in addressesWithData:
+				print(fileName + " will run")
+				runnableFiles.append(fileName)
+			else:
+				print(fileName + " won't run")
+				nonRunnableFiles.append(fileName)
+		else:
+			fcFileCount = fcFileCount + 1
+			
+with open('runnableFiles.txt', 'w') as f:
+	for fileName in runnableFiles:
+		f.write(fileName)
 
-process = os.popen(command)
-result = process.read()
-process.close();
-
-addressesWithData = []
-jumpToAddr = ""
-for line in result.splitlines():
-	if (line.startswith("@")):
-		addrString = line.split(":")[0].replace("@", "")
-		addressesWithData.append(int(addrString, 8))
-	if (".JumpTo" in line):
-		jumpToAddr = int(line.split(".JumpTo")[1], 8)
-		
-
-if jumpToAddr in addressesWithData:
-	print("will run")
-else:
-	print("won't run")
+with open('nonRunnableFiles.txt', 'w') as f:
+	for fileName in nonRunnableFiles:
+		f.write(fileName)
 	
-	
+print(str(len(runnableFiles)) + " runnable files\n")
+print(str(len(nonRunnableFiles)) + " non-runnable files\n")
+print(str(fcFileCount) + " .fc files \n")
