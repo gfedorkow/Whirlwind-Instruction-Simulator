8c523c43074e320d860e47fdc46797a7b6ebceec
Brian White <Brian.white@umb.edu>
2025-10-20 15:52:45 -0400

    working on runnable filter

--- BW-Dev/RunnableFilter/runnableFilter.py
+++ BW-Dev/RunnableFilter/runnableFilter.py
@@ -9,46 +9,66 @@ Created on Wed Sep 24 15:37:13 2025
 import os
 
 runnableFiles = []
+noJumpToFiles = []
 nonRunnableFiles = []
-fcFileCount = 0
+fcFiles = []
 
 with open('uniqueFileNames.txt', 'r') as f:
 	for fileName in f:
 		# .fc files are not readable by the disasm
 		if (not ".fc" in fileName):
-			command = "python3 ../../Py/Disassembler/wwdisasm.py -o - " + fileName
+			fileName = fileName.rstrip()
 			
-			process = os.popen(command)
-			result = process.read()
-			process.close();
-			
-			addressesWithData = []
-			jumpToAddr = ""
-			for line in result.splitlines():
-				if (line.startswith("@")):
-					addrString = line.split(":")[0].replace("@", "")
-					addressesWithData.append(int(addrString, 8))
-				if (".JumpTo" in line):
-					jumpToAddr = int(line.split(".JumpTo")[1], 8)
-					
-			
-			if jumpToAddr in addressesWithData:
-				print(fileName + " will run")
-				runnableFiles.append(fileName)
+			# first, see if the .xcore file has a JumpTo line in it 
+			# since the disassembler adds a default JumpTo 0o040 to all files
+			# if there's a real '%JumpTo' then log that address
+			jumpToAddr = 0
+			with open(fileName) as file:
+				for line in file:
+					if ('JumpTo' in line):
+						jumpToAddr = int(line.split("%JumpTo")[1], 8)
+						
+			if (jumpToAddr != 0):
+				#there's a real jump to address; now see if it's runnable
+				command = "python3 ../../Py/Disassembler/wwdisasm.py -o - " + fileName + "\n"
+				
+				process = os.popen(command)
+				result = process.read()
+				process.close();
+				
+				addressesWithData = []
+				for line in result.splitlines():
+					if (line.startswith("@")):
+						addrString = line.split(":")[0].replace("@", "")
+						addressesWithData.append(int(addrString, 8))
+						
+				if jumpToAddr in addressesWithData:
+					print(fileName + " will run")
+					runnableFiles.append(fileName)
+				else:
+					nonRunnableFiles.append(fileName)
 			else:
-				print(fileName + " won't run")
-				nonRunnableFiles.append(fileName)
+				noJumpToFiles.append(fileName)
 		else:
-			fcFileCount = fcFileCount + 1
+			fcFiles.append(fileName)
 			
 with open('runnableFiles.txt', 'w') as f:
 	for fileName in runnableFiles:
-		f.write(fileName)
+		f.write(fileName + '\n')
 
 with open('nonRunnableFiles.txt', 'w') as f:
 	for fileName in nonRunnableFiles:
-		f.write(fileName)
+		f.write(fileName + '\n')
+		
+with open('noJumpToFiles.txt', 'w') as f:
+	for fileName in noJumpToFiles:
+		f.write(fileName + '\n')
+
+with open('fcFiles.txt', 'w') as f:
+	for fileName in fcFiles:
+		f.write(fileName + '\n')
 	
 print(str(len(runnableFiles)) + " runnable files\n")
 print(str(len(nonRunnableFiles)) + " non-runnable files\n")
-print(str(fcFileCount) + " .fc files \n")
+print(str(len(noJumpToFiles)) + " files without a JumpTo in the .xcore\n")
+print(str(len(fcFiles)) + " .fc files \n")
