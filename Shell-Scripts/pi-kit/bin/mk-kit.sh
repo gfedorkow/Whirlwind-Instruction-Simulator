cfecd2b0c3c1e0b469496b7beb5f349d3a6b8300
Guy C Fedorkow <fedorkow@mit.edu>
2025-12-24 15:26:36 -0500

    starting to put a bit of automation around moving to a new RasPi instance by copying in the starter files

---
+++ Shell-Scripts/pi-kit/bin/mk-kit.sh
@@ -0,0 +1,19 @@
+
+# mk-kit copies a bunch of base files needed to install the WW Sim on a new RasPi
+# The kit assumes that we're starting with a working system...
+# Guy Fedorkow, Dec 24, 2025
+
+GPATH=~/History-of-Computing/Whirlwind/GitHub/Shell-Scripts
+
+cd ~ 
+cp .vimrc  .viminfo  .nanorc .bash_profile .gitconfig  $GPATH/pi-kit
+
+cd ~/bin
+cp auto-ping  no-blank  pinger.sh  run-ww  wwasm  ww-shell  wwsim  $GPATH/pi-kit/bin
+
+cd
+cp  ~/config-ww $GPATH/pi-kit/config-ww
+
+cp ~/History-of-Computing/Whirlwind/GitHub/.git/config  $GPATH/pi-kit/History-of-Computing/Whirlwind/GitHub/.git/
+
+cp ~/History-of-Computing/Whirlwind/GitHub/Hardware/Micro-Whirlwind/.git/config  $GPATH/pi-kit/History-of-Computing/Whirlwind/Hardware/Micro-Whirlwind/GitHub/.git/
