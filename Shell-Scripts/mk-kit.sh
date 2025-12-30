cc1bbcf0daa55eb98f1d1563d491ef4c8dec47dd
Guy C Fedorkow <fedorkow@mit.edu>
2025-12-30 08:41:06 -0500

    adding more bits and pieces for configuring a new RasPi

--- Shell-Scripts/mk-kit.sh
+++ Shell-Scripts/mk-kit.sh
@@ -6,7 +6,7 @@
 GPATH=~/History-of-Computing/Whirlwind/GitHub/Shell-Scripts
 
 cd ~ 
-cp .vimrc  .viminfo  .nanorc .bash_profile .gitconfig  $GPATH/pi-kit
+cp .basrc .vimrc  .viminfo  .nanorc .bash_profile .gitconfig  $GPATH/pi-kit
 
 cd ~/bin
 cp auto-ping  no-blank  pinger.sh  run-ww  wwasm  ww-shell  wwsim  $GPATH/pi-kit/bin
@@ -17,3 +17,5 @@ cp -r  ~/config-ww $GPATH/pi-kit/config-ww
 cp ~/History-of-Computing/Whirlwind/GitHub/.git/config  $GPATH/pi-kit/History-of-Computing/Whirlwind/GitHub/.git/
 
 cp ~/History-of-Computing/Whirlwind/Hardware/Micro-Whirlwind/GitHub/.git/config  $GPATH/pi-kit/History-of-Computing/Whirlwind/Hardware/Micro-Whirlwind/GitHub/.git/
+
+sudo cp -p -r /home/ww-demo $GPATH/pi-kit/
